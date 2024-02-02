08b5bb876ab0360c21b02c943d6238e890215952
Davidooff <david.ter.18@gmail.com>
2024-02-02 04:05:41 -0500

    headers chosing

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt::format};
 use serde_json::Value;
+
+fn get_digits(raw_digits: &String) -> Vec<u32> {
+  let digits: Vec<u32> = raw_digits.chars()
+        .filter(|c| c.is_digit(10)) // Filter out non-digit characters
+        .map(|c| c.to_digit(10).unwrap()) // Convert chars to u32
+        .collect(); 
+  digits
+}
 
 #[tauri::command]
 fn convert_filds(raw_filds: String) -> String {
   let mut fields : HashMap<String, String> = HashMap::new();
+  let mut headers : HashMap<String, String> = HashMap::new();
   let res = serde_json::from_str::<HashMap<String, Value>>(&raw_filds);
   match res {
       Ok(parsed_fields) => {
@@ -17,8 +26,14 @@ fn convert_filds(raw_filds: String) -> String {
                 Ok(parsed_values) => {
                   let value = parsed_values.get("w");
                     if let Some(value) = value {
-                      fields.insert(key, value.as_str().unwrap().to_string());
+                      fields.insert(key.clone(), value.as_str().unwrap().to_string());
                       println!("Value parsed successfuly {}", value.to_string());
+
+                      // chacking is it a labels(headers)
+                      let key_dig = get_digits(&key);
+                      if key_dig.len() == 1 && key_dig[0] == 1 {
+                          headers.insert(key, value.as_str().unwrap().to_string());
+                      }
                   } else {
                       println!("Value not found");
                   }
@@ -30,7 +45,9 @@ fn convert_filds(raw_filds: String) -> String {
       }
       Err(e) => panic!("Failed to parse JSON: {:?}", e),
   }
-  let json_string = serde_json::to_string(&fields).unwrap();
+  let fields: String = serde_json::to_string(&fields).unwrap();
+  let headers: String = serde_json::to_string(&headers).unwrap();
+  let json_string = format!("{{\"fields\": {}, \"headers\": {}}}", fields, headers);
   println!("{}", json_string);
   json_string
 }
--- src/components/drag-and-drop/DragAndDrop.tsx
+++ src/components/drag-and-drop/DragAndDrop.tsx
@@ -2,25 +2,76 @@ import { invoke } from "@tauri-apps/api";
 import { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
 import { read } from "xlsx";
+import "./DragAndDrop.scss";
 
 const fileTypes = ["TXT", "XLS"];
 
+interface Headers {
+  [key: string]: string;
+}
+
+interface Fields {
+  [key: string]: string;
+}
+interface ConvertedFields {
+  headers: Headers;
+  fields: Fields;
+}
+
 function DragAndDrop() {
-  const [file, setFile] = useState<File | null>(null);
+  const [file, setFile] = useState<ConvertedFields | null>(null);
+  const [headers, setHeaders] = useState<Headers>({});
   const handleChange = async (file: File) => {
     console.log(file);
     let fields = read(await file.arrayBuffer()).Sheets;
     fields = Object.values(fields)[0];
-    // delete fields["!margins"];
-    // delete fields["!ref"];
-    // delete fields["!autofilter"];
-
     const rawFilds = JSON.stringify(fields);
-    invoke("convert_filds", { rawFilds });
-    setFile(file);
+    let formated_fields: string | ConvertedFields = await invoke<string>(
+      "convert_filds",
+      { rawFilds }
+    );
+    formated_fields = JSON.parse(formated_fields) as ConvertedFields;
+    console.log(formated_fields);
+
+    setFile(formated_fields);
   };
   return (
-    <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+    <>
+      {file == null && (
+        <FileUploader
+          handleChange={handleChange}
+          name="file"
+          types={fileTypes}
+        />
+      )}{" "}
+      {file && (
+        <div className="chose-headers">
+          Chose headers:{" "}
+          <div>
+            {Object.keys(file.headers).map((el: string, index) => (
+              <div>
+                <input
+                  type="checkbox"
+                  key={index}
+                  name={el}
+                  onChange={(event) => {
+                    if (headers && event.target.name in headers) {
+                      delete headers[event.target.name];
+                      setHeaders({ ...headers });
+                    } else {
+                      headers[el] = file.headers[el];
+                      setHeaders({ ...headers });
+                    }
+                  }}
+                />{" "}
+                {file.headers[el]}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+      {Object.keys(headers).length ?? headers}
+    </>
   );
 }
 
